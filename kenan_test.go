ba37bfb1392e18aa93dba4d217a9006144b7e965
Louis <louismax@yeah.net>
2024-12-30 10:42:57 +0800

    暂存

--- kenan_test.go
+++ kenan_test.go
@@ -1,6 +1,8 @@
 package kenan
 
 import (
+	"github.com/louismax/kenan/core"
+	"github.com/louismax/kenan/kTool"
 	"testing"
 	"time"
 )
@@ -15,4 +17,40 @@ func TestInit(t *testing.T) {
 		return
 	}
 
+}
+
+func TestInterfaceToStr(t *testing.T) {
+	t.Log(kTool.InterfaceToStr(123))
+}
+
+func TestInterfaceToInt64(t *testing.T) {
+	t.Log(kTool.InterfaceToInt64(123))
+}
+
+func TestMakeYearDaysRand(t *testing.T) {
+	t.Log(kTool.MakeYearDaysRand(0))
+}
+func TestGetRandomString(t *testing.T) {
+	t.Log(kTool.GetRandomString(16))
+}
+func TestGetTagFieldName(t *testing.T) {
+	t.Log(kTool.GetTagFieldName(core.Params{}))
+}
+func TestMapStringToStruct(t *testing.T) {
+	type TestStruct struct {
+		AAA int `json:"aaa"`
+	}
+	obj := TestStruct{}
+	t.Log(kTool.MapStringToStruct(map[string]string{"aaa": "123"}, &obj, true))
+}
+func TestStructConvertMapByTag(t *testing.T) {
+	t.Log(kTool.StructConvertMapByTag(core.Params{}))
+}
+
+func TestComplexAnalysis(t *testing.T) {
+	type TestStruct struct {
+		AAA int `json:"aaa"`
+	}
+	obj := TestStruct{}
+	t.Log(kTool.ComplexAnalysis(map[string]string{"aaa": "123"}, &obj))
 }
